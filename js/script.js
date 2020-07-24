5c48a5af11f15af7415e7cafe06147c8ef2b4f7c
adrianvaida <ady.vaida.av@gmail.com>
2020-07-24 09:30:25 +0300

    bug fix

--- js/script.js
+++ js/script.js
@@ -1,24 +1,40 @@
 const imageContainer = document.querySelector('#image-container');
+
 const loader = document.querySelector('#loader');
 
 // Unsplash Api
 
-const count = 3;
+const count = 30;
 const apiKey = 'T0OXqxnma5zd854LpwvBWxJ3rWbHSbRg2hbY0Lm_leM';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 let photosArray = [];
 
+function setAtributes(elemnt, atributes) {
+    for (const key in atributes) {
+        elemnt.setAttribute(key, atributes[key]);
+    }
+}
+
 function displayPhotos() {
     photosArray.forEach((photo) => {
-        const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
+        const item = document.createElement('A');
+        // item.setAttribute('href', photo.links.html);
+        // item.setAttribute('target', '_blank');
 
-        const img = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular);
-        img.setAttribute('alt', photo.alt_description);
-        img.setAttribute('title', photo.alt_description);
+        setAtributes(item, {
+            href: photo.links.html,
+            target: '_blank'
+        })
 
+        const img = document.createElement('IMG');
+        // img.setAttribute('src', photo.urls.regular);
+        // img.setAttribute('alt', photo.alt_description);
+        // img.setAttribute('title', photo.alt_description);
+        setAtributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        })
         item.appendChild(img);
         imageContainer.appendChild(item);
     });
@@ -28,7 +44,7 @@ function displayPhotos() {
 async function getPhotosFromUnsplash() {
     try {
         const response = await fetch(apiUrl);
-        const photosArray = await response.json();
+        photosArray = await response.json();
 
         displayPhotos();
     } catch (error) {
